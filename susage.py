968623e913f28c8f33653cf312d47fb76debfac1
dani3lfrenc <danielfranchini@virgilio.it>
2024-06-17 13:06:46 +0200

    feat: title modified

--- susage.py
+++ susage.py
@@ -9,13 +9,13 @@ import platform
 def resize_terminal():
     current_os = platform.system()
     if current_os == 'Linux' or current_os == 'Darwin':
-        os.system('resize -s 30 130')
+        os.system('resize -s 50 160')
     elif current_os == 'Windows':
-        os.system('mode con: cols=130 lines=30')
+        os.system('mode con: cols=160 lines=50')
 
 def check_terminal_size(stdscr):
     w_rows, w_cols = stdscr.getmaxyx()
-    if w_cols < 130 or w_rows < 20:
+    if w_cols < 160 or w_rows < 50:
         resize_terminal()
         return False
 
@@ -23,31 +23,52 @@ def check_terminal_size(stdscr):
 
 def draw_title(stdscr, color):
     title = [
-        " ___ _   _ ___  __ _  __ _  ___ ",
-        "/ __| | | / __|/ _` |/ _` |/ _ \\",
-        "\__ \ |_| \__ \ (_| | (_| |  __/",
-        "|___/\__,_|___/\__,_|\__, |\___|",
-        " @dani3lfrenc        |___/      ",
+        "                                                                                                    ",
+        "                                                                                           .        ",
+        "                          ____    _   _   _ ____    _    ____ _____                   .::^!J7.      ",
+        "                         / ___|  / \ | | | / ___|  / \  / ___| ____|               .^!?JJYJYYYY?!.   ",
+        "                         \___ \ / _ \| | | \___ \ / _ \| |  _|  _|               .~?JJJJ????JJY5PY.  ",
+        "                          ___) / ___ \ |_| |___) / ___ \ |_| | |___            :!?JJ??????JJYYY5PG7  ",
+        "   .^~!777!~^.           |____/_/   \_\___/|____/_/   \_\____|_____|       .:~?JJJ??????JJJYY55PGB~  ",
+        "  ^JYYJJJJJJJJ?~.                                                      .^!?JJJJ??????JYJYY55PGBP~   ",
+        " .Y5YYJJJJ???JJJJ7~^:..                                           .:^!7?JJ????????JJYYY555PGBBJ.    ",
+        " .YGP5YYYJ??????JJJJJJ?7!~^:..                            ..:^~!7?JJJJJ???????JJJJYY555PGBGBP~     ",
+        "  :5BP555YYJJJ?77????JJJJJJJ???7!!~^^::......:::^^~~!!!!7???J??JJJJ??????77??JJYY555PGBGY^       ",
+        "   .?GBGP555YYYJ???7!!777????JJJJJJJJJJ??????JJJJJJJJJJ??JJ???777777???J?JYYYYY55PPGBBGJ.         ",
+        "     .JPBBGPP555YYYYJ???!!!!!!7777???????????????????777777!!!!!77JJJJYYY5555PPGBBP?.            ",
+        "        ~J5BBBGPP5555YYYJJ???77!!!!!!!!!~!~~~!~~~!!~!!!7???JJJYYYYYY5555PPGGBBBP?~.               ",
+        "           ^7YPBBBGGPPPP55555YYYJJYJJJJ??JJJJJJJJJJJJYYYY5555555PPPPGGBBB#BPJ7^.                  ",
+        "              :~?PGB#BBBGGPPP555555555555555555555555PPPPGGGBB##BBG5J!~.                          ",
+        "                 .^!?J5PGBBBBBBBBGGGGGGGGGGGGGGGGBBBBBB##BBGP5J?!^:.                             ",
+        "                          .:^~7?Y5PPGGGGBBGGGGGGGPPP5YJ?7!~^:.                                    ",
+        "                                    ...........                                                      ",
         ""
     ]
+    
 
     w_rows, w_cols = stdscr.getmaxyx()
+
+    # Calcoliamo la posizione iniziale per centrare orizzontalmente
     start_x = (w_cols - len(title[0])) // 2
 
+    # Stampiamo il titolo riga per riga, in cima alla finestra
     for i, line in enumerate(title):
-        stdscr.addstr(i, start_x, line, color | curses.A_BOLD)
+        stdscr.addstr(i, start_x, line, curses.color_pair(1) | curses.A_BOLD)
 
-    stdscr.addstr(i + 1, 0, "")
+    stdscr.refresh()
 
+    
 def draw_content(stdscr):
     # GET CPU, MEM and NET usage
     cpu_usage = psutil.cpu_percent(percpu=True)
     mem_usage = psutil.virtual_memory()
     net_usage = psutil.net_io_counters(pernic=False, nowrap=True)
+    processes = list(psutil.process_iter(['pid', 'name', 'username']))
 
     draw_title(stdscr, curses.color_pair(1))
 
     cpu_start_row, cpu_start_col = stdscr.getyx()
+    cpu_start_col = 0
     cpu_start_row += 2
 
     #------------------
@@ -60,6 +81,8 @@ def draw_content(stdscr):
 
     total_cpu_usage = psutil.cpu_percent()
     stdscr.addstr(cpu_start_row + 2 + len(cpu_usage) + 1, cpu_start_col + 2, f"CPU total use: {total_cpu_usage:.2f}%")
+    cpu_final_row = cpu_start_row + 2 + len(cpu_usage) + 3 
+
 
     #------------------
     # Draw the MEM info
@@ -100,6 +123,20 @@ def draw_content(stdscr):
     ipAdd = socket.gethostbyname(hostname)
     stdscr.addstr(net_start_row + 6, net_start_col, f"Local IP address: {ipAdd}")
 
+    # Draw the PROCESSES info
+    proc_start_row = cpu_final_row
+    proc_start_col = 1
+    stdscr.addstr(proc_start_row, proc_start_col, "PROCESSES", curses.A_BOLD | curses.color_pair(1))
+    for i, proc in enumerate(processes[cpu_final_row:], start=proc_start_row + 2):
+        if i >= stdscr.getmaxyx()[0] - 2:
+            break
+        pid = proc.info['pid']
+        name = proc.info['name']
+        username = proc.info['username']
+        stdscr.addstr(i, proc_start_col, f"{pid:5}")
+        stdscr.addstr(i, proc_start_col + 10, f"{name}")
+        stdscr.addstr(i, proc_start_col + 40, f"{username}")
+
 def draw_screen(stdscr):
     stdscr.clear()
     curses.curs_set(0)
@@ -117,6 +154,7 @@ def draw_screen(stdscr):
 
         if not check_terminal_size(stdscr):
             stdscr.addstr(0, 0, "Resizing terminal to at least 100x20...", curses.color_pair(1))
+            check_terminal_size(stdscr)
         else:
             draw_content(stdscr)
             # Draw the bottom bar
