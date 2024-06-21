3c19517b258be4a0f78026380b81a19841cef6a8
Daniel Franchini <73396382+dani3lfrenc@users.noreply.github.com>
2024-06-21 16:04:34 +0200

    feat: easy installation for linux and MacOS

--- setup.sh
+++ setup.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-FILE_NAME="susage"
+PYTHON_SCRIPT="susage"
 
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(pwd)"
 
 
-FILE_PATH="${SCRIPT_DIR}/${FILE_NAME}.py"
-
-DEST_DIR="/usr/local/bin"
+SCRIPT_PATH="$SCRIPT_DIR/$PYTHON_SCRIPT.py"
 
 
-if [ ! -d "$DEST_DIR" ]; then
-    exit 1
+if [[ ! -f "$SCRIPT_PATH" ]]; then
+  echo "Error: The file $SCRIPT_PATH does not exist."
+  exit 1
 fi
 
 
-check_platform() {
-    platform=$(uname)
-    if [[ "$platform" == "Linux" || "$platform" == "Darwin" ]]; then
-        pip3 install curses
-    else
-        exit 1
-    fi
-}
-
-check_platform
-pip install -r "${SCRIPT_DIR}/requirements.txt"
+chmod +x "$SCRIPT_PATH"
 
 
-chmod +x "$FILE_PATH"
+cat << EOF | sudo tee /usr/local/bin/$PYTHON_SCRIPT
+#!/bin/bash
+python3 $SCRIPT_PATH "\$@"
+EOF
 
-sudo mv "$FILE_PATH" "$DEST_DIR/$FILE_NAME"
 
-echo "Now you can execute the command: $FILE_NAME"
+sudo chmod +x /usr/local/bin/$PYTHON_SCRIPT
+
+
+pip3 show psutil &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+  echo "psutil is not installed. Installing..."
+  pip3 install psutil
+else
+  echo "psutil is already installed."
+fi
+
+echo "Setup complete. You can now run the script by typing '$PYTHON_SCRIPT' from anywhere in the terminal."
